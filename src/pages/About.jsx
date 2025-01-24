64149654f5089055a070d15074533ee40c139af0
Vivek Kumar <vivekvkc55@gmail.com>
2025-01-24 13:49:48 +0530

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,11 +6,11 @@ const About = () => {
     <div className="container-fluid bg-black text-white vh-100 d-flex justify-content-center align-items-center">
       <div className="container px-md-5">
         <div className='row g-5'>
-          <div className='col-lg-6'>
+          <div className='col-lg-6' style={{ height: "500px", overflowY: "scroll" }}>
             <div className="">
               <div className="">
                 <h1 className="gradient-text">16+</h1>
-                <p>YEARS OF EXPERIENCE</p>
+                <p className="fw-bold">YEARS OF EXPERIENCE</p>
                 <p>
                   With over 16 years in the industry, we bring a wealth of experience
                   and innovation to every project, ensuring solutions that stand the
@@ -19,7 +19,7 @@ const About = () => {
               </div>
               <div className="">
                 <h1 className="gradient-text">1600+</h1>
-                <p>PROJECT DELIVERED</p>
+                <p className="fw-bold">PROJECT DELIVERED</p>
                 <p>
                   Our proven track record of delivering 1600+ successful projects
                   speaks to our commitment to excellence and client satisfaction.
@@ -27,7 +27,33 @@ const About = () => {
               </div>
               <div className="">
                 <h1 className="gradient-text">650+</h1>
-                <p>CLIENTS SERVED</p>
+                <p className="fw-bold">CLIENTS SERVED</p>
+                <p>
+                  From startups to global enterprises, we've proudly partnered with
+                  650+ clients in 14+ countries, building lasting relationships and
+                  impactful solutions.
+                </p>
+              </div>
+              <div className="">
+                <h1 className="gradient-text">16+</h1>
+                <p className="fw-bold">YEARS OF EXPERIENCE</p>
+                <p>
+                  With over 16 years in the industry, we bring a wealth of experience
+                  and innovation to every project, ensuring solutions that stand the
+                  test of time.
+                </p>
+              </div>
+              <div className="">
+                <h1 className="gradient-text">1600+</h1>
+                <p className="fw-bold">PROJECT DELIVERED</p>
+                <p>
+                  Our proven track record of delivering 1600+ successful projects
+                  speaks to our commitment to excellence and client satisfaction.
+                </p>
+              </div>
+              <div className="">
+                <h1 className="gradient-text">650+</h1>
+                <p className="fw-bold">CLIENTS SERVED</p>
                 <p>
                   From startups to global enterprises, we've proudly partnered with
                   650+ clients in 14+ countries, building lasting relationships and
@@ -55,7 +81,7 @@ const About = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div >
     // <div className="container-fluid bg-black text-white vh-100 d-flex justify-content-center align-items-center">
     //   <h1 className="display-4 text-center gradient-text">
     //     About Alfasquare Software Solution
