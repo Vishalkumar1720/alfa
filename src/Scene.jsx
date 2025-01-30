79007a27ece5dfcc29f4170634675949cb8d24ed
Vishal kumar <vishalsinghsaini1720@gmail.com>
2025-01-30 22:49:00 +0530

    fixing issue for githup pages

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 
 export default function Scene(props) {
-  const { scene, animations } = useGLTF("/models/scene.gltf");
+  const modelPath = process.env.PUBLIC_URL + "/models/scene.gltf"; // ✅ Correct path for GitHub Pages
+  const { scene, animations } = useGLTF(modelPath);
   const { actions } = useAnimations(animations, scene);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (actions && Object.keys(actions).length > 0) {
       actions[Object.keys(actions)[0]].play(); // Play first animation
     }
