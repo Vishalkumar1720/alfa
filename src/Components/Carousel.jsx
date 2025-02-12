8a7aabd4a2d564dcbd5e7916aab7dcbee9944d50
Vishal kumar <vishalsinghsaini1720@gmail.com>
2025-02-13 00:13:28 +0530

    import carousel images

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import logo from "../Assets/images/Alfasq  logo.png"; 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -28,15 +28,71 @@ export default function Carousel() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide><div className='h-full w-full bg-dark'><h1>i am greed</h1></div></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <div className="d-flex justify-content-center align-items-center h-100 w-100">
+            <div className="row w-100">
+              {/* Left Side - Text Content */}
+              <div className="col-md-6 d-flex flex-column justify-content-center align-items-start px-4">
+                <h2 className="fw-bold">I am heading title</h2>
+                <p className="text-muted">I am a paragraph describing the content.</p>
+              </div>
+
+              {/* Right Side - Image */}
+              <div className="col-md-6 d-flex justify-content-center align-items-center">
+                <img
+                  src={logo}
+                  alt="Image description"
+                  className="img-fluid rounded"
+                  style={{ maxWidth: "100%", height: "auto",filter: "invert(1) grayscale(1)" }}
+                />
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="d-flex justify-content-center align-items-center h-100 w-100">
+            <div className="row w-100">
+              {/* Left Side - Text Content */}
+              <div className="col-md-6 d-flex flex-column justify-content-center align-items-start px-4">
+                <h2 className="fw-bold">I am heading title</h2>
+                <p className="text-muted">I am a paragraph describing the content.</p>
+              </div>
+
+              {/* Right Side - Image */}
+              <div className="col-md-6 d-flex justify-content-center align-items-center">
+                <img
+                  src={logo}
+                  alt="Image description"
+                  className="img-fluid rounded"
+                  style={{ maxWidth: "100%", height: "auto",filter: "invert(1) grayscale(1)" }}
+                />
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="d-flex justify-content-center align-items-center h-100 w-100">
+            <div className="row w-100">
+              {/* Left Side - Text Content */}
+              <div className="col-md-6 d-flex flex-column justify-content-center align-items-start px-4">
+                <h2 className="fw-bold">I am heading title</h2>
+                <p className="text-muted">I am a paragraph describing the content.</p>
+              </div>
+
+              {/* Right Side - Image */}
+              <div className="col-md-6 d-flex justify-content-center align-items-center">
+                <img
+                 src={logo}
+                 alt="Image description"
+                 className="img-fluid rounded"
+                 style={{ maxWidth: "100%", height: "auto",filter: "invert(1) grayscale(1)" }}
+                />
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+
       </Swiper>
     </>
   );
