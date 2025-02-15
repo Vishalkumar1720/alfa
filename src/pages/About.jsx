dedf09b94ed6c953e695c9538532fbdb3acd62eb
Vivek Kumar <vivekvkc55@gmail.com>
2025-02-15 20:09:27 +0530

    animation fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,19 +14,19 @@ const About = () => {
         <div className="col-lg-6" style={{ height: "500px", overflowY: "scroll" }}>
       <div className="text-white">
         <div className="mb-4">
-          <h1 className="gradient-text"><AnimatedCounter maxNumber={2} duration={500} suffix="+" /></h1>
+          <AnimatedCounter maxNumber={2} duration={500} suffix="+" />
           <p className="fw-bold">Years of Experience</p>
           <p>With over two years in the industry, we bring expertise and innovation to every project, ensuring long-lasting and impactful solutions.</p>
         </div>
 
         <div className="mb-4">
-          <h1 className="gradient-text"><AnimatedCounter maxNumber={20} duration={500} suffix="+" /></h1>
+          <AnimatedCounter maxNumber={20} duration={500} suffix="+" />
           <p className="fw-bold">Projects Delivered</p>
           <p>Our commitment to excellence is reflected in 20+ successfully delivered projects, each tailored to meet our clients' unique needs.</p>
         </div>
 
         <div className="mb-4">
-          <h1 className="gradient-text"><AnimatedCounter maxNumber={15} duration={500} suffix="+" /></h1>
+          <AnimatedCounter maxNumber={15} duration={500} suffix="+" />
           <p className="fw-bold">Clients Served</p>
           <p>From startups to established businesses, we've partnered with 15+ clients, building trust and delivering real value.</p>
         </div>
